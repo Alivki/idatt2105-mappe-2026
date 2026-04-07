7b8a800aa34174c16ebd71de2d6c5135ac90cc66
Piranavan Visvalingam <piranavan.visvalingam@outlook.com>
2026-04-07 21:13:30 +0200

    Fixing errors

--- frontend/src/components/ui/collapsible/__tests__/Collapsible.spec.ts
+++ frontend/src/components/ui/collapsible/__tests__/Collapsible.spec.ts
@@ -1,6 +1,6 @@
 import { describe, expect, it } from 'vitest'
 import { mount } from '@vue/test-utils'
-import { defineComponent, nextTick, ref } from 'vue'
+import { defineComponent, nextTick } from 'vue'
 
 import Collapsible from '../Collapsible.vue'
 import CollapsibleTrigger from '../CollapsibleTrigger.vue'
